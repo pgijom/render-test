42e2e46eb8b325800282924785eea9a6a88d72e3
Guillaume Petitat <guillaume.petitat@orckestra.com>
2026-05-26 08:53:32 -0400

    Add front-end DIST folder + some configs

---
+++ part3/Phonebook/backend/README.md
@@ -0,0 +1,3 @@
+## Phonebook backend (3.10)
+https://phonebook-p3.onrender.com/api/persons
+(it make take some time to wake up)
--- part3/Phonebook/backend/index.js
+++ part3/Phonebook/backend/index.js
@@ -3,6 +3,7 @@ const app = express();
 const morgan = require('morgan');
 
 app.use(express.json());
+app.use(express.static('dist'));
 
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 
